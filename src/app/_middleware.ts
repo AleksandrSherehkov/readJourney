ef85f0cc95d987f4b7b6fb1564cbdf763a8ac0bc
Aleksandr Shereshkov <101018177+AleksandrSherehkov@users.noreply.github.com>
2024-02-28 10:50:05 +0200

    fix

--- src/app/_middleware.ts
+++
@@ -1,8 +0,0 @@
-// app/_middleware.ts
-
-import { NextResponse } from 'next/server';
-
-export default function middleware(request) {
-    console.log('Cookies:', request.cookies);
-    return NextResponse.next();
-}
