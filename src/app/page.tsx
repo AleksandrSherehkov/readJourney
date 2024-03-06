ce7a97db4dbe399554018963c72843458be79856
Aleksandr Shereshkov <101018177+AleksandrSherehkov@users.noreply.github.com>
2024-03-06 23:16:39 +0200

    add page error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,11 @@ export default function Home() {
     return (
         <>
             <Header />
-            <main className="flex flex-col items-center justify-between"></main>
+            <main className="flex flex-col items-center justify-between p-5">
+                <h1 className="mt-[50%] text-center text-[32px] font-bold uppercase leading-8 tracking-titleForm text-fogWhite md:mt-[30%] md:text-[64px] md:leading-[60px] md:tracking-titleFormTablet xl:mt-[25%]">
+                    Welcome <span className="text-grey">read journey</span>
+                </h1>
+            </main>
         </>
     );
 }
